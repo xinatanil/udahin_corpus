bcc326d7e6832e9b81cd3e42e15078797a57ee1a
Aibek Dzhakipov <xinatanil@gmail.com>
2023-01-27 05:28:34 +0600

    Script updates

--- remove_weird_formatting.py
+++ remove_weird_formatting.py
@@ -8,25 +8,11 @@ with open ('letter_wip.xml', 'r' ) as f:
 # replace weird newline and 8 spaces in source
     content_new = re.sub('\n               ', r' ', content, flags = re.M)
 
-# remove my mistake with global search and replace
-# <p>южн.</p>
-#         </blockquote>
-    content_new = re.sub('<p>южн.</p>\n\t\t</blockquote>', r'южн.</blockquote>', content_new, flags = re.M)
-
-# remove consequences of previous command
-# <blockquote>
-#            южн.</blockquote>
-    content_new = re.sub('<blockquote>\n\t\t\tюжн.</blockquote>', r'<blockquote>южн.</blockquote>', content_new, flags = re.M)
-    
-#        <blockquote>
-#            <p>южн.</p>
-#        </blockquote>
-# to
-# <blockquote><p>южн.</p></blockquote>
-#    content_new = re.sub('<blockquote>\n\t\t\t<p>южн.<\/p>\n\t\t<\/blockquote>', r'<meta>южн.</meta>', content_new, flags = re.M)
-
     content_new = re.sub('<p>южн\.</p>', r'южн.', content_new, flags = re.M)
     content_new = re.sub('<p>фольк\.</p>', r'фольк.', content_new, flags = re.M)
+
+    content_new = re.sub('южн\.\n\t\t</blockquote>', r'южн.</blockquote>', content_new, flags = re.M)
+    content_new = re.sub('<blockquote>\n\t\t\tюжн\.', r'<blockquote>южн.', content_new, flags = re.M)
     
     outputFile = open("letter_after.xml", "w")
     outputFile.write(content_new)
