0da92afed735019a1f38b7d6582abf7c15fd425b
Aibek Dzhakipov <xinatanil@gmail.com>
2023-03-10 22:45:14 +0600

    Update paths

--- initial_script.sh
+++ initial_script.sh
@@ -1,6 +1,6 @@
-python3 remove_weird_formatting.py letter_wip.xml letter_after.xml
+python3 scripts/remove_weird_formatting.py letter_wip.xml letter_after.xml
 
-saxon -xsl:detect_homonyms.xsl -s:letter_after.xml -o:letter_after.xml
+saxon -xsl:scripts/detect_homonyms.xsl -s:letter_after.xml -o:letter_after.xml
 sed -i '' 's/homonym/<homonym>/g' letter_after.xml
 sed -i '' 's/closingHomonym/<\/homonym>/g' letter_after.xml
 
@@ -9,7 +9,7 @@ xmllint --format - < letter_after.xml > after_linting.xml
 rm letter_after.xml
 mv after_linting.xml letter_after.xml
 
-saxon -xsl:detect_lexical_meanings.xsl -s:letter_after.xml -o:letter_after.xml
+saxon -xsl:scripts/detect_lexical_meanings.xsl -s:letter_after.xml -o:letter_after.xml
 sed -i '' 's/meaning/<meaning>/g' letter_after.xml
 sed -i '' 's/closingMeaning/<\/meaning>/g' letter_after.xml
 
@@ -18,7 +18,7 @@ xmllint --format - < letter_after.xml > after_linting.xml
 rm letter_after.xml
 mv after_linting.xml letter_after.xml
 
-python3 python_script.py letter_after.xml letter_after.xml
+python3 scripts/python_script.py letter_after.xml letter_after.xml
 
 export XMLLINT_INDENT=$'\t'
 xmllint --format - < letter_after.xml > after_linting.xml
