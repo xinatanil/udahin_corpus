9ccc3a991ff54d0269da0e06a772e8c4a9e1283e
Aibek Dzhakipov <xinatanil@gmail.com>
2023-05-02 00:40:23 +0600

    Fix wrong script name

--- scripts/copyCardsThatStartOnLetter.sh
+++ scripts/copyCardsThatStartOnLetter.sh
@@ -1,2 +1,2 @@
-saxon -xsl:playground.xsl -s:../kg_ru_yudahin.xml -o:../letter_wip.xml
+saxon -xsl:copyCardsThatStartOnLetter.xsl -s:../kg_ru_yudahin.xml -o:../letter_wip.xml
 cp ../letter_wip.xml ../letter_backup.xml 
