a32c2ffb20fe6bbc115e123e1f364a525bd75fd1
xinatanil <xinatanil@gmail.com>
2023-05-29 22:48:41 +0600

    Script updates

--- scripts/iterative.sh
+++ scripts/iterative.sh
@@ -1,8 +1,8 @@
-python3 scripts/python_script.py letter_wip.xml letter_after.xml
+python3 python_script.py ../letter_wip.xml ../sources/letter_after.xml
 
 export XMLLINT_INDENT=$'\t'
-xmllint --format - < letter_after.xml > after_linting.xml
-rm letter_after.xml
-mv after_linting.xml letter_after.xml
+xmllint --format - < ../sources/letter_after.xml > ../sources/after_linting.xml
+rm ../sources/letter_after.xml
+mv ../sources/after_linting.xml ../sources/letter_after.xml
 
-ksdiff letter_wip.xml letter_after.xml
+ksdiff ../letter_wip.xml ../sources/letter_after.xml
