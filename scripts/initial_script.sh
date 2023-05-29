4beb8569bc79bd67c7a1183484286b0ebd701467
xinatanil <xinatanil@gmail.com>
2023-05-29 22:41:25 +0600

    Script updates

--- scripts/initial_script.sh
+++ scripts/initial_script.sh
@@ -1,28 +1,28 @@
-python3 scripts/remove_weird_formatting.py letter_wip.xml letter_after.xml
+python3 remove_weird_formatting.py ../letter_wip.xml ../sources/letter_after.xml
 
-saxon -xsl:scripts/detect_homonyms.xsl -s:letter_after.xml -o:letter_after.xml
-sed -i '' 's/homonym/<homonym>/g' letter_after.xml
-sed -i '' 's/closingHomonym/<\/homonym>/g' letter_after.xml
-
-export XMLLINT_INDENT=$'\t'
-xmllint --format - < letter_after.xml > after_linting.xml
-rm letter_after.xml
-mv after_linting.xml letter_after.xml
-
-saxon -xsl:scripts/detect_lexical_meanings.xsl -s:letter_after.xml -o:letter_after.xml
-sed -i '' 's/meaning/<meaning>/g' letter_after.xml
-sed -i '' 's/closingMeaning/<\/meaning>/g' letter_after.xml
+saxon -xsl:detect_homonyms.xsl -s:../sources/letter_after.xml -o:../sources/letter_after.xml
+sed -i '' 's/homonym/<homonym>/g' ../sources/letter_after.xml
+sed -i '' 's/closingHomonym/<\/homonym>/g' ../sources/letter_after.xml
 
 export XMLLINT_INDENT=$'\t'
-xmllint --format - < letter_after.xml > after_linting.xml
-rm letter_after.xml
-mv after_linting.xml letter_after.xml
+xmllint --format - < ../sources/letter_after.xml > ../sources/after_linting.xml
+rm ../sources/letter_after.xml
+mv ../sources/after_linting.xml ../sources/letter_after.xml
 
-python3 scripts/python_script.py letter_after.xml letter_after.xml
+saxon -xsl:detect_lexical_meanings.xsl -s:../sources/letter_after.xml -o:../sources/letter_after.xml
+sed -i '' 's/meaning/<meaning>/g' ../sources/letter_after.xml
+sed -i '' 's/closingMeaning/<\/meaning>/g' ../sources/letter_after.xml
 
 export XMLLINT_INDENT=$'\t'
-xmllint --format - < letter_after.xml > after_linting.xml
-rm letter_after.xml
-mv after_linting.xml letter_after.xml
+xmllint --format - < ../sources/letter_after.xml > ../sources/after_linting.xml
+rm ../sources/letter_after.xml
+mv ../sources/after_linting.xml ../sources/letter_after.xml
 
-ksdiff letter_wip.xml letter_after.xml
+python3 python_script.py ../sources/letter_after.xml ../sources/letter_after.xml
+
+export XMLLINT_INDENT=$'\t'
+xmllint --format - < ../sources/letter_after.xml > ../sources/after_linting.xml
+rm ../sources/letter_after.xml
+mv ../sources/after_linting.xml ../sources/letter_after.xml
+
+ksdiff ../letter_wip.xml ../sources/letter_after.xml
