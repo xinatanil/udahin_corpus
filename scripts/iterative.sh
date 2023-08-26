da856aea6432afc3c42c9c7a34c9936499ac56a8
xinatanil <xinatanil@gmail.com>
2023-08-26 18:21:42 +0600

    Move source file to variable

--- scripts/iterative.sh
+++ scripts/iterative.sh
@@ -1,8 +1,9 @@
-python3 python_script.py ../letter_wip.xml ../sources/letter_after.xml
+SOURCE_FILE=../letter_wip.xml
+python3 python_script.py $SOURCE_FILE ../sources/letter_after.xml
 
 export XMLLINT_INDENT=$'\t'
 xmllint --format - < ../sources/letter_after.xml > ../sources/after_linting.xml
 rm ../sources/letter_after.xml
 mv ../sources/after_linting.xml ../sources/letter_after.xml
 
-ksdiff ../letter_wip.xml ../sources/letter_after.xml
+ksdiff $SOURCE_FILE ../sources/letter_after.xml
