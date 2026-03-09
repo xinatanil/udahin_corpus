f2d060112243329a32241c473e9443ecb8f4cf24
xinatanil <xinatanil@gmail.com>
2026-03-10 03:16:39 +0600

    Add hardcoded cases

--- scripts/identify_alternative_forms.py
+++ scripts/identify_alternative_forms.py
@@ -14,6 +14,48 @@ content_new = re.sub(
     flags=re.M
 )
 
+content_new = content_new.replace(
+    '<blockquote>(неправ. вместо аа)</blockquote>',
+    '<alternativeForm>(неправ. вместо аа)</alternativeForm>'
+)
+content_new = content_new.replace(
+    '<blockquote>(неправ. вместо кун)</blockquote>',
+    '<alternativeForm>(неправ. вместо кун)</alternativeForm>'
+)
+
+content_new = re.sub(
+    r'<blockquote>(\(неправ\.\s+(?!вместо\b)[^)]+\))</blockquote>',
+    r'<alternativeForm>\1</alternativeForm>',
+    content_new,
+    flags=re.M
+)
+
+deepr_cases = [
+    '(деепр. бүйүрүп и бүйрүп)',
+    '(деепр. жаап)',
+    '(деепр. прош. вр. жумуп и жуумп)',
+    '(деепр. ийирип, ийрип)',
+    '(деепр. каап)',
+    '(деепр. кээп)',
+    '(деепр. кооп)',
+    '(деепр. көөмп)',
+    '(деепр. көөп)',
+    '(деепр. сээп)',
+    '(деепр. таамп)',
+    '(деепр. таап)',
+    '(деепр. тээп)',
+    '(деепр. чаап и редко чабып)',
+    '(деепр. чүйрүп и чүйүрүп)',
+    '(деепр. прош. вр. үйүрүп или үйрүп)',
+    '(деепр. өөп; см. жытта- 2)'
+]
+
+for dc in deepr_cases:
+    content_new = content_new.replace(
+        f'<blockquote>{dc}</blockquote>',
+        f'<alternativeForm>{dc}</alternativeForm>'
+    )
+
 count = content_new.count('<alternativeForm>')
 print(f'Total alternativeForm tags: {count}')
 
